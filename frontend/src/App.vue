b4ad64a55af7ee0459b61d1242a32fd682bc99e1
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-16 01:48:32 +0000

    Add questions view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    
+    <!-- TODO: delete test code -->
+    <p>{{ ans.age }}</p>
+    <p v-if="!ageValidator">年齢を入力してください</p>
+    <!-- ====================== -->
+
+    <form id="form" method="POST">
+      <p>
+        <label for="age">年齢:</label>
+        <input type="text" id="age" v-model="ans.age">
+      </p>
+      <p>
+        <label for="sex">性別:</label>
+        <input type="text" id="sex" v-model="ans.sex">
+      </p>
+
+      <input type="submit" value="送信">
+    </form>
   </div>
 </template>
 
@@ -24,7 +27,20 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      ans: {
+        age: '',
+        sex: '',
+      },
+      validation: {
+        age: false,
+        sex: false
+      }
+    }
+  },
+  computed: {
+    ageValidator:function() {
+      this.validation.age = !!this.ans.age;
+      return this.validation.age;
     }
   }
 }
@@ -38,23 +54,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
