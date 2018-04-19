978c0501a245975fbcd6fbc5c455b80aeb8395b6
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-19 02:19:31 +0000

    Update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '252%)8rffttdrhku^k_^)3amo!c7+5yu))ckf5focp4q&^ld5l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
