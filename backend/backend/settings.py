79e02968d8f06a834cddf03ee61fecb6272caace
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2018-04-19 15:36:36 +0900

    Modified for PostgreSQL Join

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': '078kobedb',                    
+        'USER': '078kobe',            
+        'PASSWORD': '078kobe',             
+        'HOST': 'localhost',                
+        'PORT': '5432',  
     }
 }
 
