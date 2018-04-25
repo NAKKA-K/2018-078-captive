d7d0dccad5c60159baeab3b38c16726341a44cd6
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-25 04:26:34 +0000

    Add i18n

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,17 @@ import App from './App.vue'
 import Resource from 'vue-resource'
 Vue.use(Resource);
 
+import VueI18n from 'vue-i18n'
+Vue.use(VueI18n)
+
+const data = require('./i18n/messages.json')
+const i18n = new VueI18n({
+  locale: 'en',
+  messages: data
+})
+
 new Vue({
   el: '#app',
+  i18n: i18n,
   render: h => h(App)
 })
